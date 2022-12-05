3ad680e6ea6e8ddaf2544426b48630e9543196eb
sanneck <neck_315@hotmail.com>
2022-12-05 20:51:50 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Youtube-to-mp3
+
+This is a simple python script created with the GPT-3 chatbot, it had some bugs and didn't do everything I asked for so some of the code is mine. It downloads a youtube video and converts it to mp3. It also displays a fixed-length progress bar with a percentage indicator and shows the total time taken to download the youtube video.
+
+## Requirements
+
+* Python 3.x
+
+If you have any problem try to install what is below:
+* youtube-dl (install using pip install youtube-dl)
+* ffmpeg (install using pip install ffmpeg)
+
+## Usage:
+
+`$ python youtube_to_mp3.py [youtube_video_url]`
+
+The script will download the youtube video as an mp3 file in the highest possible quality and save it 
+in the Downloads folder located in this same directory. It will also display a progress bar with a percentage indicator as the download is in progress. When the download is complete, it will display the total time taken to download the youtube video.
+
+## TODO:
+
+- [ ] Accept both `[youtube_video_url]` or `[youtube_video_ID]`
+- [ ] Download 1 or more file from list in external file
+- [ ] Select audio quality
+- [ ] Add lyrics?
---
+++ youtube_to_mp3.py
@@ -0,0 +1,98 @@
+import youtube_dl
+import time
+import os
+import sys
+
+# Get current working directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+download_folder = current_dir + '/Downloads/'
+
+try:
+        
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+except OSError:
+    pass
+
+os.chdir(os.path.dirname(download_folder))
+
+# Define a custom class that extends the base YoutubeDL class and adds a progress bar
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+class ProgressBar(object):
+    def __init__(self, total_length, filled_char="=", empty_char=" "):
+        self.total_length = total_length
+        self.filled_char = filled_char
+        self.empty_char = empty_char
+        self.reset()
+
+    def reset(self):
+        self.filled_length = 0
+
+    def update(self, progress):
+        progress = float(progress.split('%')[0])
+        num_filled = int(progress)
+        num_empty = self.total_length - num_filled
+
+        if num_filled < self.filled_length:
+            self.reset()
+
+        self.filled_length = num_filled
+        filled_str = self.filled_char * self.filled_length
+        empty_str = self.empty_char * num_empty
+        print("\r[{}{}] {}%".format(filled_str, empty_str, progress), end="")
+
+class CustomYoutubeDL(youtube_dl.YoutubeDL):
+    def __init__(self, *args, **kwargs):
+        super(CustomYoutubeDL, self).__init__(*args, **kwargs)
+        self.progress_bar = ProgressBar(100)
+        self.add_progress_hook(self.on_progress_hook)
+
+    def on_progress_hook(self, d):
+        if d["status"] == "downloading":
+            self.progress_bar.update(d["_percent_str"])
+
+# Define the youtube-dl options
+ydl_opts = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(title)s.%(ext)s",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            #"preferredquality": "320",
+        }
+    ],
+    "logger": MyLogger(),
+    "progress_hooks": [],
+}
+
+# Record the start time
+start = time.time()
+
+# Get the youtube video URL from the command-line arguments
+if len(sys.argv) != 2:
+    print("Usage: python youtube_to_mp3.py [youtube_video_url]")
+    sys.exit(1)
+
+video_url = sys.argv[1]
+
+print("\nDownloading..\n")
+# Download the youtube video as an mp3
+with CustomYoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_url])
+
+# Record the end time and calculate the total time taken
+end = time.time()
+total_time = end - start
+
+# Display the total time taken
+print("\nTotal time taken: {} seconds".format(round(total_time, 1)))
